90d60ba9bec3a5165c3a0eafa71d4740de84b72a
Samuel Panjaitan <samuelpanjaitan914@gmail.com>
2026-06-02 09:08:01 +0700

    menghapus yang ga penting

--- app/Models/Destinasi.php
+++ app/Models/Destinasi.php
@@ -6,13 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Destinasi extends Model
 {
-    // Menggunakan nama tabel tanpa 's'
+    
     protected $table = 'destinasi';
 
     protected $fillable = [
         'nama',
         'slug',
-        'kategori_id', // <--- UBAH INI: Tambahkan '_id' karena kita pakai relasi
+        'kategori_id', 
         'lokasi',
         'deskripsi',
         'sejarah',
@@ -23,17 +23,13 @@ class Destinasi extends Model
         'gambar'
     ];
 
-    // ---------------------------------------------------
-    // RELASI BARU: Ke tabel Kategori (Dari Langkah 3)
-    // ---------------------------------------------------
+   
     public function kategori()
     {
         return $this->belongsTo(Kategori::class, 'kategori_id');
     }
 
-    // ---------------------------------------------------
-    // RELASI LAMA: Ke galeri & review (Biarkan saja)
-    // ---------------------------------------------------
+    
     public function galeri()
     {
         return $this->hasMany(Galeri::class);
